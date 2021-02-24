1b66719a890dc472ceb31458d1ea50ff2f3eb579
huginn <huginn@huginn.me>
2021-02-24 20:59:25 +0100

    created modules/projects.py

---
+++ bin/modules/projects.py
@@ -0,0 +1,6 @@
+class Pm():
+    def __init_(self):
+        # check for the config file in ~/.config/.mimir.d/pm-manifest.yaml
+        # if exists skip init process
+        # if not start init process
+        pass
