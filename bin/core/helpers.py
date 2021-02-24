16dbd777ce60f20e284be9f9033844b1f2e9560e
huginn <huginn@huginn.me>
2021-02-24 20:47:21 +0100

    sub-shell init process

---
+++ bin/core/helpers.py
@@ -0,0 +1,39 @@
+import os
+from os import path
+
+
+class FileManager():
+    def file_exists(_path):
+        if _path[0] == '~':
+            return path.isfile(FileManager.__expand_path(_path))
+
+        return path.isfile(_path)
+
+    def directory_exists(_path):
+        if _path[0] == '~':
+            return path.isdir(FileManager.__expand_path(_path))
+
+        return path.isdir(_path)
+
+    def create_directory(_path):
+        full_path = _path
+        if full_path[0] == '~':
+            full_path = FileManager.__expand_path(full_path)
+
+        os.mkdir(full_path)
+
+    def try_create_file(path, content='', override=False):
+        full_path = FileManager.__expand_path(path)
+        if FileManager.file_exists(full_path):
+            if override:
+                with open(full_path, 'w') as file:
+                    file.write(content)
+                return True
+            else:
+                return False
+        with open(full_path, 'w') as file:
+            file.write(content)
+        return True
+
+    def __expand_path(_path):
+        return path.expanduser(_path)
--- bin/core/mimirshell.py
+++ bin/core/mimirshell.py
@@ -1,7 +1,29 @@
 from cmd import Cmd
+from core.helpers import FileManager
+from yamlize import Attribute, Object
 
 
 class MimirShell(Cmd):
+    def __init__(self):
+        # check if ~/.config/ exists
+        print(FileManager.directory_exists('~/.config'))
+        if FileManager.directory_exists('~/.config/') == False:
+            FileManager.create_directory('~/.config')
+            print("Created '~/.config/' directory")
+        # check if ~/.config/.mimir.d/ exists
+        if FileManager.directory_exists('~/.config/.mimir.d/') == False:
+            FileManager.create_directory('~/.config/.mimir.d')
+            print("Created '~/.config/.mimir.d/' directory")
+        # check if ~/.config/.mimirc exists if not create
+        if FileManager.file_exists('~/.config/.mimir') == False:
+            print('.mimirc not found. Starting init process')
+            config = Config()
+            config.pm_status = 'foo'
+            FileManager.try_create_file('~/.config/.mimir',
+                                        Config.dump(config))
+
+        Cmd.__init__(self)
+
     def do_quit(self, line):
         """
         exits mimir app
@@ -12,3 +34,7 @@ class MimirShell(Cmd):
 
     def help_quit(self):
         print(self.do_quit.__doc__)
+
+
+class Config(Object):
+    pm_status = Attribute(type=str, default='default')
